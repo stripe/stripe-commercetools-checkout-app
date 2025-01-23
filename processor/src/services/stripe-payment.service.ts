a6bbe5dfbc818310744d2a785a9cc226ad833358
Aaron Meza <aaron.meza@gluo.mx>
2025-01-23 07:11:41 -0600

    Update payment state to 'INITIAL' and simplify conversion logic
    If used PENDING update, the sdk.getPaymentAmount is taking as a paid amount, and return a "Cart full paid error"

--- processor/src/services/stripe-payment.service.ts
+++ processor/src/services/stripe-payment.service.ts
@@ -373,7 +373,7 @@ export class StripePaymentService extends AbstractPaymentService {
   public async processStripeEvent(event: Stripe.Event): Promise<void> {
     log.info('Processing notification', { event: JSON.stringify(event.id) });
     try {
-      const updateData = await this.stripeEventConverter.convert(event);
+      const updateData = this.stripeEventConverter.convert(event);
 
       for (const tx of updateData.transactions) {
         const updatedPayment = await this.ctPaymentService.updatePayment({
