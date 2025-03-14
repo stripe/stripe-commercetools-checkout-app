dd679fb3e29de27db945d2e8abff3166000ffed0
Aaron Meza Rodríguez <136672901+aaronmezarguez@users.noreply.github.com>
2025-03-14 13:30:56 -0600

    bug: Error when getting the getPaymentAmount from a paid cart, retrieve ammountPlanned from the payment
    Update test

--- processor/test/services/stripe-payment.service.spec.ts
+++ processor/test/services/stripe-payment.service.spec.ts
@@ -243,9 +243,6 @@ describe('stripe-payment.service', () => {
       const getPaymentMock = jest
         .spyOn(DefaultPaymentService.prototype, 'getPayment')
         .mockReturnValue(Promise.resolve(mockGetPaymentResult));
-      const getPaymentAmountMock = jest
-        .spyOn(DefaultCartService.prototype, 'getPaymentAmount')
-        .mockResolvedValue(mockGetPaymentAmount);
 
       const updatePaymentMock = jest
         .spyOn(DefaultPaymentService.prototype, 'updatePayment')
@@ -256,7 +253,6 @@ describe('stripe-payment.service', () => {
 
       expect(getCartMock).toHaveBeenCalled();
       expect(getPaymentMock).toHaveBeenCalled();
-      expect(getPaymentAmountMock).toHaveBeenCalled();
       expect(updatePaymentMock).toHaveBeenCalled();
     });
   });
