a597bfc708d428547ebf5565daa4176d415c4740
Aaron Meza <aaron.meza@gluo.mx>
2025-04-07 22:14:34 -0600

    fix: Correct section numbering in README and remove unnecessary logs in stripe-payment.service.ts

--- processor/src/services/stripe-payment.service.ts
+++ processor/src/services/stripe-payment.service.ts
@@ -554,8 +554,7 @@ export class StripePaymentService extends AbstractPaymentService {
     try {
       const query = `metadata['ct_customer_id']:'${ctCustomerId}'`;
       const customer = await stripeApi().customers.search({ query });
-      log.warn('find Stripe customer found');
-      log.warn('customer.data', JSON.stringify(customer.data, null, 2));
+
       return customer.data[0];
     } catch (e) {
       log.warn(`Error finding Stripe customer for ctCustomerId: ${ctCustomerId}`, { error: e });
